567976cc4c73b668a14c08e4e310db986ffdeb3f
jian chen <jian.chen@skymatters.com>
2017-09-22 22:19:53 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,7 @@ module Cable
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-
-
+    config.paths.add 'lib', eager_load: true
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**/}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'jobs', '{**/}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'serializers', '{**/}')]
