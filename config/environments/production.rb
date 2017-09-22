ae498f080fb9c5bfa8c3dd2d3697ef2d62ec36ed
jian chen <jian.chen@skymatters.com>
2017-09-22 22:11:19 +0800

    add stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
