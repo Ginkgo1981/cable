0445aba89a25245f6dead0865b3442c73652a8d7
jian chen <jian.chen@skymatters.com>
2018-01-29 13:49:07 +0800

    fix

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -16,34 +16,4 @@ class UserBook < ApplicationRecord
 
   belongs_to :user
   belongs_to :book
-
-%w(
-
-  oRFG9jha9C5Xt-RHhtyKyilOJg0U
-  oRFG9jsa3Y0JY2qXEvXAqebSijqA
-  oRFG9jinqI1SeT6Lutm-Q_yAs9zw
-  oRFG9jjALhvKpG20BQTFwTKNa70s
-  oRFG9jk_ph0eNNnMbraWElzw7QZA
-  oRFG9jvAvvf94LV0iMAhuU2y7NSk
-  oRFG9jrQr5eRdfx9GquZjaCcqcv4
-  oRFG9jvNXwOEus20R7tD5_SXZoas
-  oRFG9juemgsgSDABp0sPwly2QpEc
-  oRFG9jqfcuEhGdSllkq1VwtlVs9g
-  oRFG9jqBZ-zlbYvNY1EU14lU7RG0
-  oRFG9jnQ_VvGEaSzSAlgz5EtxYcM
-  oRFG9jkc7-6QXlqXIkSEJngqQe_w
-  oRFG9jjNsJNa9KXdB_VVx4duEXlM
-  oRFG9jjnhmhoKcVTfLcA-6nDDXdw
-  oRFG9jqtWUemLP5FCSkNprZVykOA
-  oRFG9jnyTspCZsDiE_5i4zXa2CxM
-  oRFG9jh1OUd5O-fLb_FYjQuUi3wY
-
-).each do |open_id|
-
-  cv = CustomValue.find_by value: open_id
-  cv.value='mocked-open-id'
-  cv.save
-
-end
-
 end
