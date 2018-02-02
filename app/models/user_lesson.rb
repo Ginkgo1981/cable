d7a7dcf735eb1026e17e302b899fdd77b563ef8b
jian chen <jian.chen@skymatters.com>
2018-02-02 14:41:09 +0800

    fix user_answer

--- app/models/user_lesson.rb
+++ app/models/user_lesson.rb
@@ -54,7 +54,7 @@ class UserLesson < ApplicationRecord
   def change_state
     if self.answers && self.answers.size > 0
       self.user.add_count(self)
-      self.state = self.reading_date == Time.now.strftime('%Y-%m-%d') ? 1 : 2
+      self.state = self.reading_date == Time.now.to_date ? 1 : 2
     end
   end
 
